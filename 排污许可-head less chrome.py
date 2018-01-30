9d1fd4c61d073f8ec1cdcae56880bccfc3f7e26b
Chaos <c410185@163.com>
2018-01-30 23:21:43 +0800

    第二次尝试

--- 排污许可-head less chrome.py
+++ 排污许可-head less chrome.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from time import sleep
+from time import clock
 
 chromedriver = 'C:\\Users\何方辉\AppData\Local\Google\Chrome\Application\chromedriver.exe'
 
@@ -11,8 +12,24 @@ prefs = {'profile.default_content_settings.popups': 0,
 options.add_experimental_option('prefs', prefs)
 
 driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=options)
-driver.get('http://sahitest.com/demo/saveAs.htm')
-driver.find_element_by_xpath('/html/body/a[3]').click()
-print(driver.title)
+driver.get('http://www.hnep.gov.cn:98/')
+com_list = driver.find_element_by_xpath('//*[@id="UpdatePanel2"]/div[3]/table/tbody')
+print(com_list.text)
+
+print('===============================================')
+
+driver.find_element_by_xpath('//*[@id="asp_enpnomon"]/table/tbody/tr/td[1]/a[3]').click()
+sleep(20)
+com_list2 = driver.find_element_by_xpath('//*[@id="UpdatePanel2"]/div[3]/table/tbody')
+print(com_list2.text)
+print('================================================')
+
+sleep(3)
+driver.find_element_by_xpath('//*[@id="asp_enpnomon"]/table/tbody/tr/td[1]/a[3]').click()
+sleep(20)
+com_list2 = driver.find_element_by_xpath('//*[@id="UpdatePanel2"]/div[3]/table/tbody')
+print(com_list2.text)
+print('================================================')
+
 sleep(3)
 driver.quit()
