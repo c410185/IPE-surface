a8f597f40ad711657170d63e533c87b90e4a9bd8
Chaos <c410185@163.com>
2018-02-03 23:52:13 +0800

    再次学习廖雪峰后尝试，先add,然后commit，之后要push

--- HTMLtopdf.py
+++ HTMLtopdf.py
@@ -1,3 +1,7 @@
-import pdfkit
+import requests
 
-pdfkit.from_url('http://baidu.com', 'out.pdf')
+req = requests.get('http://permit.mep.gov.cn/permitExt/upanddown.do?method=download&ewmfile=fbfile&datafileid=97c482627f4d4921952a93cc2562ca11')
+print(len(req.content))
+file_path = 'C:\\Users\何方辉\Downloads\\te.pdf'
+with open(file_path, 'wb') as fw:
+    fw.write(req.content)
